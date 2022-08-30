4dcdfbde52ed0ca9e29e52f086f524b3de0777db
DNagaSrinivas <112463233+DNagaSrinivas@users.noreply.github.com>
2022-08-30 13:01:25 +0530

    Add files via upload

---
+++ 3rd.java
@@ -0,0 +1,23 @@
+import java.io.*;
+  
+class Geek {
+  
+    // Function to find whether an input
+    // character is vowel or not
+    static void Vowel_Or_Consonant(char y)
+    {
+        if (y == 'a' || y == 'e' || y == 'i' || y == 'o'
+            || y == 'u')
+            System.out.println("It is a Vowel.");
+        else
+            System.out.println("It is a Consonant.");
+    }
+  
+    // The Driver code
+    static public void main(String[] args)
+    {
+        Vowel_Or_Consonant('b');
+        Vowel_Or_Consonant('u');
+    }
+}
+
---
+++ 6th.java
@@ -0,0 +1,15 @@
+
+class Naga {
+ 
+    // Main driver method
+    public static void main(String[] args)
+    {
+         // Custom integer input
+        int i = 97;
+         // Type casting character to integer
+        char ch = (char)i;
+        // Print statement
+        System.out.println(ch);
+    }
+}
+
